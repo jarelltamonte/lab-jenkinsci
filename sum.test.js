0a22eed38712aee83d802180c0966db7791e665d
Jarell <jarelltamonte@gmail.com>
2026-07-06 17:08:51 +0800

    changed 5 to 7

--- sum.test.js
+++ sum.test.js
@@ -4,6 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 5 + 7 to equal 12', () => {
-    expect(sum(5, 7)).toBe(12);
+test('adds 7 + 7 to equal 14', () => {
+    expect(sum(7, 7)).toBe(14);
 });
